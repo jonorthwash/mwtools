44a32805ff51b23691c76b8f0a803763f4ce7ade
Brendan Molloy <brendan@bbqsrc.net>
2011-07-10 00:39:53 +1000

    Have a readme.txt.

--- setup.py
+++ setup.py
@@ -9,14 +9,26 @@ from os import listdir
 setup(
 	name = "mwparser",
 	version = "0.7",
+	url = "https://github.com/bbqsrc/mwparser",
+	download_url='https://github.com/bbqsrc/mwparser/archives/master',
 	packages = ["mwparser"],
-	#scripts = scripts,
-
+	provides = ["mwparser"],
 	author = "Brendan Molloy",
 	author_email = "brendan@bbqsrc.net",
 	description = """Parses Mediawiki syntax into readable plain-text suitable
-	for linguistic purposes.
-	""",
+	for linguistic purposes.""",
 	license = "CC0",
-	keywords = "mediawiki wikipedia parser"
+	keywords = ["mediawiki", "wikipedia", "parser"],
+	classifiers=[
+    "Development Status :: 4 - Beta",
+    "Operating System :: OS Independent",
+	"License :: Public Domain",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.1",
+    "Programming Language :: Python :: 3.2",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+	"Topic :: Text Processing :: Linguistic",
+    ]
+	
 )
